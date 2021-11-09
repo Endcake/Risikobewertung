bfda0cfe515d4203d46ce76f64a943d1aa688b1e
Christopher Kren <christopher.kren@gmail.com>
2021-11-09 20:39:56 +0100

    sm 2

--- Risikobewertung/Risikobewertung/Form1.cs
+++ Risikobewertung/Risikobewertung/Form1.cs
@@ -26,7 +26,19 @@ namespace Risikobewertung
 
         private enum Trigger { btnExit}
 
-        private readonly StateMachine<state, Trigger> _machine;
+        // private readonly StateMachine<state, Trigger> _machine;
+        StateMachine<> _machine = new StateMachine<state, Trigger>(state.Open);
+
+
+                        // Instantiate a new state machine in the 'off' state
+            var onOffSwitch = new StateMachine<string, char>(off);
+
+        // Configure state machine with the Configure method, supplying the state to be configured as a parameter
+        onOffSwitch.Configure(off).Permit(space, on);
+        onOffSwitch.Configure(on).Permit(space, off);
+
+
+
 
         private void btnExit_Click(object sender, EventArgs e)
         {
